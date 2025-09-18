f14b1a51a972fb1e199c2ce63fc09709b75a7a5a
Popoye-75 <kasifmohd818@gmail.com>
2025-09-18 23:22:44 +0530

    updates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let random = Math.floor(Math.random() * 105); // Declare an varible which store random value
+// let random = Math.floor(Math.random() * 105); // Declare an varible which store random value
 let loginButton = document.getElementById("loginInterface"); // Select the login button using id
 let signUpButton = document.getElementById("signInterface"); // Select the sign up button or link using id
-let hiddenScreen = document.getElementById("hidden-box"); // Select the hidden guess number from this line
-let statusScreen = document.getElementsByClassName(".score-update"); // Status screen to show the status
+// let hiddenScreen = document.getElementById("hidden-box"); // Select the hidden guess number from this line
+// let statusScreen = document.getElementsByClassName(".score-update"); // Status screen to show the status
+// const generateRandom = (random) => {  // Function to generate random value 
+//     console.log(random); // This line is for debugging in only nolonger use in code
+//     return random; // Return random value verifying or any uses
+// }
 
 const showLogin = () => { // Function for shows the login interface and hide signup 
     loginButton.style.display = "flex"; // Enable login interface
@@ -13,17 +17,65 @@ const showSignUp = () => { // Function for shows the signup interface and hide l
     loginButton.style.display = "none";  // Disable login interface
 }
 
-const trialGame = () => { // Function for redirect on another game web
-    location = ("http://127.0.0.1:5500/numberhunt.html"); // When user press the playnow button this will redirect to game web
-}
-const generateRandom = (random) => {  // Function to generate random value 
-    console.log(random); // This line is for debugging in only nolonger use in code
-    return random; // Return random value verifying or any uses
-}
 // Start from here
-let cells = document.querySelectorAll("td"); // Select all td tag from this line 
+let choiceTime = document.querySelector(".timerBtn");
+let choiceChance = document.querySelector(".chanceBtn");
+let chanceOption = document.querySelector(".chance");
+let timerOption = document.querySelector(".time");
+
+choiceTime.addEventListener("click", (event) => {
+    event.stopPropagation();
+    timerOption.style.display = "block";
+    chanceOption.style.display = "none";
+    console.log("Timer button was clicked ..");
+});
+
+choiceChance.addEventListener("click", (event) => {
+    event.stopPropagation();
+    timerOption.style.display = "none";
+    chanceOption.style.display = "block";
+    console.log("Chance button was clicked ..");
+});
+
+// agar menus ke andar click hua toh bhi document listener na chale
+timerOption.addEventListener("click", (event) => event.stopPropagation());
+chanceOption.addEventListener("click", (event) => event.stopPropagation());
+
+document.addEventListener("click", () => {
+    chanceOption.style.display = "none";
+    timerOption.style.display = "none";
+});
+
+
+let timeBtn = document.querySelectorAll(".game-console-box-mode-userInput span");
+timeBtn.forEach(btn => {
+    btn.addEventListener("click", function () {
+        const value = btn.innerText.trim();
+        if (value === "10") {
+            console.log("ten");
+        } else if (value === "30") {
+            console.log("thir");
+        } else {
+            console.log("sixty");
+        }
+    });
+});
+
+// let timeScreen = document.getElementById("remainingtime");
+// timeScreen.innerHTML = "hello";
+// const countDown = setInterval(() => {
+//     // timeScreen.innerText = time;
+//     console.log(timeLeft);
+//     timeLeft--;
+//     if (time < 0) {
+//         clearInterval(countDown);
+//         console.log("time up");
+//     }
+// }, 1000);
+
+let cells = document.querySelectorAll(".game-console-input-keyboard td"); // Select all td tag from this line 
 cells.forEach((cell, index) => { // 
     cell.addEventListener("click", function () { // Add event listener to check which button is click
         console.log("Index: " + index + " | Value: " + cell.textContent);
-    })
+    });
 });
