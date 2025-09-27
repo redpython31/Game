8eaf454f48311b00440203d423de7cabe14f0d8c
Popoye-75 <kasifmohd818@gmail.com>
2025-09-28 00:11:21 +0530

    Add new functions

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// let random = Math.floor(Math.random() * 105); // Declare an varible which store random value
 let loginButton = document.getElementById("loginInterface"); // Select the login button using id
 let signUpButton = document.getElementById("signInterface"); // Select the sign up button or link using id
-// let hiddenScreen = document.getElementById("hidden-box"); // Select the hidden guess number from this line
-// let statusScreen = document.getElementsByClassName(".score-update"); // Status screen to show the status
-// const generateRandom = (random) => {  // Function to generate random value 
-//     console.log(random); // This line is for debugging in only nolonger use in code
-//     return random; // Return random value verifying or any uses
-// }
+let hiddenScreen = document.getElementById("hidden-box"); // Select the hidden guess number from this line
+let statusScreen = document.querySelector(".score-update"); // Status screen to show the status
 
 const showLogin = () => { // Function for shows the login interface and hide signup 
     loginButton.style.display = "flex"; // Enable login interface
@@ -46,7 +41,7 @@ document.addEventListener("click", () => {
 
 let timeBtn = document.querySelectorAll("#setTime span");
 let timeScreen = document.getElementById("remainingTime");
-let timeLeft = 10;
+let timeLeft = 30;
 
 timeBtn.forEach(btn => {    // This function for check which time button is click by user
     btn.addEventListener("click", () => {
@@ -67,10 +62,11 @@ timeBtn.forEach(btn => {    // This function for check which time button is clic
     });
 });
 
-let chanceBtm = document.querySelectorAll("#setChance span");
+let chanceBtn = document.querySelectorAll("#setChance span");
 let chanceScreen = document.getElementById("remainingChance");
 let chanceLeft = 15;
-chanceBtm.forEach(btn => {    // This funtion for check which chance button is click by user
+
+chanceBtn.forEach(btn => {    // This funtion for check which chance button is click by user
     btn.addEventListener("click", () => {
         const value = btn.innerText.trim();
         if (value === "5") {
@@ -89,20 +85,58 @@ chanceBtm.forEach(btn => {    // This funtion for check which chance button is c
     })
 });
 
+
 // Start from here
+let random = Math.floor(Math.random() * 105); // Declare an varible which store random value
+const generateRandom = () => {  // Function to generate random value 
+    return random; // Return random value verifying or any uses
+}
 
-// const countDown = setInterval(() => {
-//     console.log(timeLeft);
-//     timeLeft--;
-//     if (time < 0) {
-//         clearInterval(countDown);
-//         console.log("time up");
-//     }
-// }, 1000);
+const startGame = () => { // Start game function 
+    generateRandom(random); // Calling the random function
+    console.log(random);
+    const countDown = setInterval(() => { // Declare an countdown fucntion for time counting
+        timeScreen.innerText = timeLeft; // Print the timeLeft
+        timeLeft--; // Decrease time by 1 sec
+        if (timeLeft < 0) {
+            clearInterval(countDown); // Clear the time interval
+            timeScreen.innerText = "Time Up!"
+            console.log("Time Up !");
+        }
+    }, 1000);
 
-let cells = document.querySelectorAll(".game-console-input-keyboard td"); // Select all td tag from this line 
-cells.forEach((cell, index) => { // 
-    cell.addEventListener("click", function () { // Add event listener to check which button is click
-        console.log("Index: " + index + " | Value: " + cell.textContent);
+    let cells = document.querySelectorAll(".game-console-input-keyboard td"); // Select all td tag from this line 
+    cells.forEach((cell) => { // Iterate through each cell 
+        cell.addEventListener("click", function () { // Add event listener to check which button is click
+            // console.log("Value: " + cell.textContent);
+            this.style.backgroundColor = "red";
+            chanceLeft--; // Decrease chance by 1 after click
+            chanceScreen.innerText = chanceLeft; // Print the chance
+            if (chanceLeft <= 0) { //
+                chanceScreen.innerText = chanceLeft;
+                statusScreen.innerText = "GameOver";
+                console.log("Chance is Over");
+                cells.forEach(td => td.style.pointerEvents = "none"); // Block the cells after chance is zero
+            }
+            const value = Number(cell.textContent);
+            if (random == value) {
+                statusScreen.innerText = "Congratulations";
+                statusScreen.style.color = "green";
+                statusScreen.style.fontSize = "26px"
+                hiddenScreen.innerText = random;
+                this.style.backgroundColor = "green";
+                cells.forEach(td => td.style.pointerEvents = "none");
+                clearTimeout(countDown);
+            } else if (value < random) {
+                statusScreen.innerText = "Try greater one ";
+            } else {
+                statusScreen.innerText = "Try  Smaller one ";
+            }
+        });
     });
-});
+}
+const restartGame = () => {
+    console.log("game reset");
+}
+
+
